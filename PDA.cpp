bfde9f675cea12daf243edd0cb76e1e0b8e54a91
Kush Goliya <kgoliya@umich.edu>
2017-11-03 16:29:15 -0400

    Basic compiler added

--- PDA.cpp
+++ PDA.cpp
@@ -8,7 +8,7 @@
 #define STATE_B 2
 #define STATE_C 3
 #define END 4
-#define DEBUG false
+#define DEBUG true
 
 using namespace std;
 
@@ -20,7 +20,30 @@ using namespace std;
  char first_term = 0;
  char second_term = 0;
  char third_term = 0;
+ enum ComparisonType {LT,LTE,GT,GTE,EQ};
+ enum ComparisonValueType{IMM,COMP};
 
+ string typeName[] = {"LT","LTE","GT","GTE","EQ"};   
+ string valueTypeName[] = {"IMM","COMP"};   
+ struct Comparison{
+    ComparisonValueType comparisonValueTypeLeft;
+    int left;
+    ComparisonValueType comparisonValueTypeRight;
+    int right;
+    ComparisonType operation;
+    bool progress; //true for AND and false for OR
+    bool last;
+ };
+
+ Comparison allCompares[26];  //MAX NO of allowed conditions 
+
+ int gotoComp[26]; //TODO: make -1 initially
+
+ int numTerms = 0;
+ int compares = 0;
+ char componentsTochars[26];
+ char countTermOfComponent[26];
+ 
 bool pda(char inputChars){
     bool reset = false;
     bool compare = false;
@@ -100,12 +123,181 @@ bool run(string in){
     return match;
 }
 
+
+string removeWhiteSpaces(string in){
+    string out = "";
+
+    for(int i = 0;i<in.length();i++){
+        char ch = in.at(i);
+        if(ch != ' '){
+            out+=ch;
+        }
+    }
+    return out;
+}
+
+
+int findComponentWithInputCountChar(char ch){
+    for(int i = 0;i<numTerms;i++){
+        if(countTermOfComponent[i] == ch){
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool isAlphabet(char ch){
+    int num = (int) ch;
+    if((num <= 90 && num >= 65) || (num <= 122 && num >= 97)){
+        return true;
+    }
+    return false;
+}
+
+//restriction on regex --> cannot use compiler keychars
+//only enter char searches
+//only 26 chars allowed
+void compile(string in){
+    numTerms = 0;
+    bool seeCounter = false;
+    int i = 0;
+    for(;i<in.length();i++){
+        char indChar = in.at(i);
+        if(indChar == '('){
+            seeCounter = true;
+        }
+        else if(indChar == ')'){
+            seeCounter = false;
+            numTerms++;
+        }
+        else if(indChar == ','){
+            i++;
+            break;
+        }
+        else{ //any other character
+            if(seeCounter){
+                countTermOfComponent[numTerms] = indChar;
+            }
+            else{
+                componentsTochars[numTerms] = indChar;
+            }
+        }
+    }
+    
+    compares = 0;
+    bool processingACompare = false;
+    bool isComp;
+    for(;i<in.length();i++){
+        char ch = in.at(i);
+        cout<<"Comparison chars : "<<ch<<endl;
+        if(ch == '&'){
+            allCompares[compares].progress = true;
+            allCompares[compares].last = false;
+            compares++;
+            processingACompare = false;
+        }
+        else if(ch == '|'){
+            allCompares[compares].last = false;
+            compares++;
+            processingACompare = false;
+        }
+        else if(ch == '>'){
+            allCompares[compares].operation = GT;
+        }
+        else if(ch == '<'){
+            allCompares[compares].operation = LT;
+        }
+        else if(ch == '='){
+            char prev = in.at(i-1);
+            if(prev == '>'){
+                allCompares[compares].operation = GTE;
+            }
+            else if(prev == '<'){
+                allCompares[compares].operation = LTE;
+            }
+            else{
+                allCompares[compares].operation = EQ;
+            }
+        }
+        else{
+            if(!processingACompare){
+                processingACompare = true;
+                allCompares[compares].progress = false;
+                allCompares[compares].last = true;
+                isComp = isAlphabet(ch);
+                cout <<"isComp(Left) : "<<isComp<<endl;
+                if(isComp){
+                    allCompares[compares].comparisonValueTypeLeft = COMP;
+                    allCompares[compares].left = findComponentWithInputCountChar(ch);
+                }
+                else{
+                    allCompares[compares].comparisonValueTypeLeft = IMM;
+                    allCompares[compares].left = 0;
+                    while(i<in.length()){
+                        int num1 = (int)in.at(i) - 48;
+                        if(num1 < 0 && num1 > 9){
+                            break;
+                        }
+                        else{
+                            allCompares[compares].left = (allCompares[compares].left * 10) + num1;
+                            i++;
+                        }
+                    }
+                }
+            } // end of if(!processingACompare)
+            else{
+                isComp =  isAlphabet(ch);
+                cout <<"isComp(Right) : "<<isComp<<endl;
+                if(isComp){
+                    allCompares[compares].comparisonValueTypeRight = COMP;
+                    allCompares[compares].right = findComponentWithInputCountChar(ch);
+                }
+                else{
+                    allCompares[compares].comparisonValueTypeRight = IMM;
+                    allCompares[compares].right = 0;
+                    while(i<in.length()){
+                        int num1 = (int)in.at(i) - 48;
+                        if(num1 < 0 || num1 > 9){
+                            i--;
+                            cout<<allCompares[compares].right<<endl;
+                            break;
+                        }
+                        else{
+                            cout<<"Adding : "<<num1<<endl;
+                            allCompares[compares].right = (allCompares[compares].right * 10) + num1;
+                            i++;
+                        }
+                    }
+                }
+            } // end of else of if(!processingACompare)
+        } //end of big else
+    } //end of second for    
+    
+    compares++;
+    if(DEBUG){
+        cout<<"Detected components are : "<<endl;
+        for(int j = 0;j<numTerms;j++){
+            cout<<j<<" => char : "<<componentsTochars[j]<<"; with count term : "<<countTermOfComponent[j]<<endl;
+        }
+
+        cout<<"Comparisons Detected are : "<<endl;
+        cout<<"#Compares,leftType,leftValue,RightType,rightValue,comparison,progress,isLast"<<endl;
+        for(int j = 0;j<compares;j++){
+            Comparison c = allCompares[j];
+            cout<<j<<" , "<<valueTypeName[c.comparisonValueTypeLeft]<<" , "<<c.left<<" , "<<valueTypeName[c.comparisonValueTypeRight]<<" , "<<c.right<<" , "<<typeName[c.operation]<<" , "<<c.progress<<" , "<<c.last<<endl;
+        }
+    }
+    
+}
+
 int main(){
     string user_chars;
     ifstream inputChars;
     string expressionIn = "";
     bool match = false;
 
+    compile("0(i)1(j),i>=0&j>=0&i=j");
+    return 0;
     cout << "Enter the characters to be matched (3 only) = ";
     cin >> user_chars;
     first_term = user_chars[0];
