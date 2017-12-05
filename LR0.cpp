8a345f6610e93d4c16cda43c0ca27c21a849a446
Kush Goliya <kgoliya@umich.edu>
2017-12-04 21:27:07 -0500

    State machine creation done

--- LR0.cpp
+++ LR0.cpp
@@ -25,15 +25,29 @@ struct states{
 	unsigned countOfRules;
 	bool isEndState;				
 	bool isReductionState;
-    int gotoState;
+    
+    int gotoStates[NUM_CHAR];
+    bool isGotoStatesValid[NUM_CHAR];
+    
+
+
+    bool seenExpandedStatesForType[NUM_RULES];
 };
 
 char transition_char[NUM_CHAR];
 unsigned int num_transition_char = 0;
+char rule_type_char[NUM_CHAR];
+unsigned int num_rule_type_char = 0;
+
+
 int rule_count = 0;
 grammer lr0[NUM_RULES];
 states stt[NUM_STATES]; //State Transition Table
 int state_count = 0;
+
+int sameCharRuleCount = 0;
+
+int output[NUM_RULES];
 
 void fillTransitionChar(string str)
 {
@@ -57,6 +71,20 @@ void fillTransitionChar(string str)
 	}
 }
 
+void fillRuleType(char ch){
+    bool add_char = true;
+    for(int i = 0;i<num_rule_type_char;i++){
+        if(rule_type_char[i] == ch){
+            add_char = false;
+            break;
+        }
+    }
+    if(add_char){
+        rule_type_char[num_rule_type_char] = ch;
+        num_rule_type_char++;
+    }
+}
+
 int compile(void)
 {
 	string expressionIn = "";
@@ -72,7 +100,9 @@ int compile(void)
 			lr0[rule_count].dot_pos = 0;	//Initialize all the dot positions to 0
 
 			fillTransitionChar(lr0[rule_count].rule); //Collect all the new characters 
-			rule_count++;
+			fillRuleType(lr0[rule_count].type);
+            rule_count++;
+
 	    }
 
 	    cout << transition_char << endl;
@@ -110,21 +140,6 @@ string printRuleWithDot(grammer inputRule){
 }
 
 bool checkForEquivalentStates(states in1, states in2){
-/*struct states{
-    unsigned int state_num;		// Number assigned to this state, such as S0, S1, etc
-	grammer currentRules[NUM_RULES]; // What rules this states hold
-	int next_states[NUM_CHAR]; 	// The states that are child of this
-	bool isValid[NUM_CHAR];
-	unsigned countOfRules;
-	bool isEndState;				
-	bool isReductionState;
-    int gotoState;
-};
-struct grammer{
-	char type; 		// This is left side of the rule
-	string rule; 		// This is the right side of the rule
-	unsigned int dot_pos;	// Tells where the dot is in the current grammer
-};*/
     if(in1.countOfRules != in2.countOfRules){
         return false;
     }
@@ -147,7 +162,7 @@ struct grammer{
 }
 
 unsigned int giveEquivalentStateNumber(states in){  //do for every new state created to join equivalent states, returns equivalent state number
-    for(int i = 0;i<state_count;i++){
+    for(int i = state_count - 1;i>=0;i--){
         if(stt[i].state_num != in.state_num && checkForEquivalentStates(stt[i],in)){
             return i;
         }
@@ -155,6 +170,114 @@ unsigned int giveEquivalentStateNumber(states in){  //do for every new state cre
     return -1;
 }
 
+int* rulesWithSameTypeAsChar(char in){
+    sameCharRuleCount = 0;
+    for(int i = 0;i<rule_count;i++){
+        if(lr0[i].type == in){
+            output[sameCharRuleCount] = i;
+            sameCharRuleCount++;
+        }
+    }
+    return output;
+}
+
+bool addMoreRulesWithChar(char in){
+    for(int i = 0;i<rule_count;i++){
+        if(lr0[i].type == in){
+            return true;
+        }
+    }
+    return false;
+}
+
+int ruleTypePosInArray(char ch){
+    for(int i = 0;i<num_rule_type_char;i++){
+        if(rule_type_char[i] == ch){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void generateEquivalentRules(states* inputState){
+    int i = 0;
+    while(i<inputState->countOfRules){
+        grammer currentRule = inputState->currentRules[i];
+        if(currentRule.rule.length() > currentRule.dot_pos){
+            char nextChar = currentRule.rule.at(currentRule.dot_pos);   //The next char of the new rule that must be considered for making a new rule
+            if(addMoreRulesWithChar(nextChar)){
+                int pos = ruleTypePosInArray(nextChar);
+                if(pos >= 0 && !inputState->seenExpandedStatesForType[pos]){
+                    int* ruleNumbersToAdd = rulesWithSameTypeAsChar(nextChar);
+                    for(int j = 0;j<sameCharRuleCount;j++){
+                        inputState->currentRules[inputState->countOfRules] = lr0[ruleNumbersToAdd[j]];
+                        inputState->countOfRules++;
+                    }
+                    inputState->seenExpandedStatesForType[pos] = true;
+                }
+            }
+        }
+        i++;
+    }
+}
+
+void combineEquivalentStates(){
+    int newState = 0;
+    for(int i = state_count-1;i>=0;i--){
+        newState = giveEquivalentStateNumber(stt[i]);
+        if(newState != -1){
+            state_count--;
+            for(int j = 0;j<state_count;j++){
+                for(int k = 0;k<num_transition_char;k++){
+                    if(stt[j].isValid[k] && stt[j].next_states[k] == i){
+                        stt[j].next_states[k] = newState;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+int giveCharPositionInTransitionCharArray(char ch){
+    for(int i = 0;i<num_transition_char;i++){
+        if(transition_char[i] == ch){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void generateGotoStates(){
+/*struct states{
+    unsigned int state_num;		// Number assigned to this state, such as S0, S1, etc
+	grammer currentRules[NUM_RULES]; // What rules this states hold
+	int next_states[NUM_CHAR]; 	// The states that are child of this
+	bool isValid[NUM_CHAR];
+	unsigned countOfRules;
+	bool isEndState;				
+	bool isReductionState;
+    
+    int gotoStates[NUM_CHAR];
+    bool isGotoStatesValid[NUM_CHAR];
+    
+
+
+    bool seenExpandedStatesForType[NUM_RULES];
+};*/
+    for(int i = 0;i<state_count;i++){
+        for(int j = 0;j<num_transition_char;j++){
+            if(stt[i].isValid[j] && stt[stt[i].next_states[j]].isReductionState){
+                char createdRuleType = stt[stt[i].next_states[j]].currentRules[0].type;
+                int nextStateNumberForNewType = giveCharPositionInTransitionCharArray(createdRuleType);
+                if(nextStateNumberForNewType != -1 && stt[i].isValid[nextStateNumberForNewType]){
+                    stt[i].gotoStates[j] = stt[i].next_states[nextStateNumberForNewType];
+                    stt[i].isGotoStatesValid[j] = true;
+                }
+            }
+        }
+    }
+}
 
 void createTable(void)
 {
@@ -167,7 +290,7 @@ void createTable(void)
 	state_count++;
 
 	//Start exapnading the state diagram
-	for(int itr = 0; itr <=state_count; itr++)
+	for(int itr = 0; itr <state_count; itr++)
 	{   //itr is the current state which is being processed
 		//cout << "**** State " << itr << endl;
 		if(!stt[itr].isEndState)
@@ -210,6 +333,8 @@ void createTable(void)
                                     stt[state_count].isEndState = false;
                                 }
                                 state_count++;
+                                generateEquivalentRules(&stt[state_count-1]);
+                                
                             }
                             else
                             {
@@ -219,6 +344,7 @@ void createTable(void)
                                 stt[nextValidState].currentRules[rulesInValidState] = stt[itr].currentRules[i];
                                 stt[nextValidState].currentRules[rulesInValidState].dot_pos++;
                                 stt[nextValidState].countOfRules++;
+                                generateEquivalentRules(&stt[nextValidState]);
                             }
                             break;
                         }
@@ -226,14 +352,13 @@ void createTable(void)
 				}
 			}
 		}
-		else
-		{
-			//cout << "End State reached at " << itr << endl;
-			continue; 
-		}
+        combineEquivalentStates();
 	}
+    generateGotoStates();
 
-	for(int i = 0;i<=state_count;i++){
+    cout<<"Number of Generated States : "<<state_count<<endl;
+	for(int i = 0;i<state_count;i++){
+		cout << "************************************"<<endl;
 		cout << "State "<<i<<endl;
 		states currentState = stt[i];
 		cout << "Rules "<<endl;
@@ -245,7 +370,13 @@ void createTable(void)
 		cout << "Next States for chars  "<<endl;
 		for(int j = 0;j<num_transition_char;j++){
 			if(currentState.isValid[j]){
-				cout<<"		"<<transition_char[j]<<" : "<<currentState.next_states[j]<<endl;
+				cout<<"		"<<transition_char[j]<<" : "<<currentState.next_states[j];
+                if(currentState.isGotoStatesValid[j]){
+                    cout<<"("<<currentState.gotoStates[j]<<")"<<endl;
+                }
+                else{
+                    cout<<endl;
+                }
 			}
 		}
         if(currentState.isReductionState){
@@ -261,7 +392,7 @@ void createTable(void)
 int main(){
 
 	compile();
-    	createTable();
+    createTable();
 	
 	return 0;
 }
